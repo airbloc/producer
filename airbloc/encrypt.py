71d68fa05dfc885f28edf7e23fb8e0b3a875be4f
Vista <retail3210@gmail.com>
2018-07-11 15:38:43 +0900

    Proxy Re-Encryption preparation with pyUmbral

---
+++ airbloc/encrypt.py
@@ -0,0 +1,29 @@
+from typing import Optional, Tuple
+
+from umbral import pre, config, keys, signing
+from umbral.curve import SECP256K1
+
+config.set_default_curve(SECP256K1)
+
+class Encryptor:
+    def __init__(self, encryption_key: bytes, signing_key: bytes):
+        self.private_key = keys.UmbralPrivateKey.from_bytes(encryption_key)
+        self.public_key = self.private_key.get_pubkey()
+
+        self.signing_key = keys.UmbralPrivateKey.from_bytes(signing_key)
+        self.verify_key = self.signing_key.get_pubkey()
+        self.signer = signing.Signer(private_key=self.private_key)
+    
+    def encrypt(self, data: bytes) -> Tuple[bytes, pre.Capsule]:
+        cipher, capsule = pre.encrypt(self.public_key, data)
+        return cipher, capsule
+    
+    def reencrypt(self, public_key: bytes) -> pre.KFrag:
+        pubkey = keys.UmbralPublicKey.from_bytes(public_key)
+
+        kfrags = pre.split_rekey(delegating_privkey=self.private_key,
+                signer=self.signer,
+                receiving_pubkey=pubkey,
+                threshold=10,
+                N=20)
+        return kfrags
