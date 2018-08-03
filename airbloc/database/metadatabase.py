aa009af7481e2c98ef41057cc9f210f1e520e72a
Vista <retail3210@gmail.com>
2018-08-03 18:53:55 +0900

    Add create to Metadatabase

--- airbloc/database/metadatabase.py
+++ airbloc/database/metadatabase.py
@@ -2,12 +2,20 @@ from airbloc.database.bigchaindb import BigchainDBConnection
 
 class Metadatabase:
 
-    def __init__(self):
-        self.conn = BigchainDBConnection()
+    def __init__(self, conn: BigchainDBConnection):
+        self._bdb = conn
+
+    def create(self, type: str, payload: dict) -> str:
+        created_at = self._bdb.create({
+            'type': type,
+            'payload': payload,
+        })
+        return created_at
 
     def get(self, type: str, id: str) -> dict:
         query = {
             'data.type': type,
-            '_id': id
+            'id': id,
         }
-        return self.conn.retrieve_one(query)
+        data = self._bdb .retrieve_one(query)
+        return data['payload'] if data else None
