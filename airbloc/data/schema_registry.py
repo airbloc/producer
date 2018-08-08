818da746a5f6f9b5c78755792aac420fa5d1f241
Vista <retail3210@gmail.com>
2018-08-08 14:56:36 +0900

    Register schema name to schema registry

--- airbloc/data/schema_registry.py
+++ airbloc/data/schema_registry.py
@@ -5,20 +5,31 @@ from airbloc.database import Metadatabase
 class SchemaRegistry(object):
 
     def __init__(self, metadb: Metadatabase):
-        self._cache = dict()
+        self._validator_cache = dict()
         self._metadb = metadb
 
-    def register(self, schema: dict) -> str:
+    def register(self, name, schema: dict) -> str:
         # TODO: make payable
         validator = fastjsonschema.compile(schema)
-        schema_id = self._metadb.create('Schema', {'schema': schema})
-        self._cache[schema_id] = validator
+
+        payload = {
+            'name': name,
+            'schema': schema,
+        }
+        schema_id = self._metadb.create('Schema', payload)
+        self._validator_cache[schema_id] = validator
         return schema_id
+
+    def list(self, query=None, limit=-1, page=0):
+        if query is None:
+            query = {}
+
+        return self._metadb.query(query, limit=limit, page=page)
 
     def validate(self, category_id, data) -> object:
         """ Validate given data. """
 
-        if category_id not in self._cache:
+        if category_id not in self._validator_cache:
             # fetch schema from metadatabase (blockchain, BigchainDB)
             category_of_app = self._metadb.get('CategoryOfApp', category_id)
             schema = self._metadb.get('Schema', category_of_app['schemaId'])
@@ -26,7 +37,7 @@ class SchemaRegistry(object):
             # TODO: check that schema is registered in blockchain
 
             validator = fastjsonschema.compile(schema['schema'])
-            self._cache[category_id] = validator
+            self._validator_cache[category_id] = validator
 
-        validator = self._cache[category_id]
+        validator = self._validator_cache[category_id]
         return validator(data)
